498feb476e0f8207609498d79d8e8bf940bc6927
blues <linwts@mail.com>
2021-11-18 12:28:25 +0800

    Add offer_model

--- models/transformer/offer_model.py
+++ models/transformer/offer_model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-from models.transformer import modules, modeling, base_bert
+from models.transformer import modules, base_bert
 
 
 class OfferModel(tf.Module):
@@ -16,13 +16,39 @@ class OfferModel(tf.Module):
         self.dtype = dtype
         self.is_training = is_training
 
-    # @tf.Module.with_name_scope
-    # def get_contrastive_loss(self, pooled, ):
+    @tf.Module.with_name_scope
+    def get_contrastive_loss(self, pooled, pos_pair_idx, temp=1.0):
+        """
+        Args:
+        pooled: shape 2D [bsz, d_model]
+            pooled logits.
+        pos_pair_idx: shape 2D [any, 2]
+            positive pair index in batch.
+            - ex: [0, 1] indicate pooled[0, :] and pooled[1, :] are positive pair.
+        temp: scalar
+            temperature for softmax.
+        """
+        normalize_logits = tf.math.l2_normalize(pooled, axis=-1)
+        # shape [bsz, bsz]
+        self_cossim = tf.einsum("id,jd->ij", normalize_logits, normalize_logits)
+        # temperature for softmax
+        self_cossim = self_cossim / temp
+
+        # shape [any]
+        pos_cos = tf.gather_nd(self_cossim, pos_pair_idx)
+        # shape [any, bsz]
+        others_cos = tf.gather_nd(self_cossim, pos_pair_idx[:, 0:1])
+
+        # cross-entropy
+        softmax_denominator = tf.reduce_logsumexp(others_cos, axis=1)
+
+        contrastive_loss = -1 * (pos_cos - softmax_denominator)
+
+        return contrastive_loss
 
     @tf.Module.with_name_scope
-    def get_mlm_loss(self, output, target, mlm_pos_padded):
-        masked_target = target * mlm_pos_padded
-        loss, logits = self.encoder.masked_lm_loss(output, masked_target)
+    def get_mlm_loss(self, output, target):
+        loss, logits = self.encoder.masked_lm_loss(output, target)
 
         return loss
 
